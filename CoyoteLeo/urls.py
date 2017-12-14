f05e1df2a730f3c679566625ce04714c457d9369
YouEn <karta09104@gmail.com>
2017-12-14 19:39:00 +0800

    init

---
+++ CoyoteLeo/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+                      url(r'^__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
